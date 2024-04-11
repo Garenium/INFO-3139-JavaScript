a0b87385e79e8d37707b03c9db00f6cc404ede2c
Garen Ikezian <gifanshawe@gmail.com>
2024-04-11 01:38:20 -0400

    fix fetching user lists from server

---
+++ nodeExercises/Labs/Project2/app.js
@@ -0,0 +1,66 @@
+import { port } from "./config.js";
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+import * as socketHandlers from "./socketHandlers.js";
+import matColours from "./matdes100colours.json" assert { type: "json" };
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+const httpServer = http.createServer(app);
+const io = new Server(httpServer);
+
+//set up admin color first
+let coloridx = Math.floor(Math.random() * matColours.colours.length);
+let adminColor = matColours.colours[coloridx];
+console.log(`ADMIN COLOUR: ${adminColor}`);
+
+io.on("connection", (socket) => {
+  console.log("new connection established");
+
+  //Scenario 1 - client connects to the server
+  socket.on("join", (clientData) => {
+    socketHandlers.handleJoin(socket, clientData);
+  });
+
+  // scenario 2 - client disconnects from server
+  socket.on("disconnect", () => {
+    socketHandlers.handleDisconnect(socket);
+  });
+
+  //scenario 3 - client sends notification that user started typing
+  socket.on("typing", (clientData) => {
+    socketHandlers.handleTyping(socket, clientData);
+  });
+
+  // scenario 4 - client sends message to room including self
+  socket.on("message", (clientData) => {
+    socketHandlers.handleMessage(io, socket, clientData);
+  });
+});
+
+// Handle 404 errors
+app.use((req, res, next) => {
+  const error = new Error("No such route found");
+  error.status = 404;
+  next(error);
+});
+
+// Error handler middleware
+app.use((error, req, res, next) => {
+  res.status(error.status || 500).send({
+    error: {
+      status: error.status || 500,
+      message: error.message || "Internal Server Error",
+    },
+  });
+});
+
+httpServer.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
+
+export default adminColor;
---
+++ nodeExercises/Labs/Project2/socketHandlers.js
@@ -0,0 +1,104 @@
+import matColours from "./matdes100colours.json" assert { type: "json" };
+const users = [];
+import adminColor from "./app.js";
+import moment from "moment";
+
+const handleJoin = async (socket, clientData) => {
+  const room = clientData.roomName;
+  const name = clientData.chatName;
+
+  const isNameTaken = users.some((user) => user.name === name);
+
+  if (isNameTaken) {
+    socket.emit("nameexists", `${name} is already taken.`);
+  } else {
+    //Set the color for the user
+    let coloridx = Math.floor(Math.random() * matColours.colours.length);
+    let color = matColours.colours[coloridx];
+
+    users.push({ socketId: socket.id, name: name, room: room, color: color });
+
+    socket.emit("welcome", {
+      text: ` Welcome ${name}.`,
+      authortime: `Admin Says @${moment().format("h:mm:ss a")}`,
+      color: adminColor,
+    });
+
+    await socket.join(room);
+
+    await socket.to(room).emit("someonejoined", {
+      text: `${name} has joined the \"${room}\" room.`,
+      authortime: `Admin Says @${moment().format("h:mm:ss a")}`,
+      color: adminColor,
+    });
+  }
+};
+
+const handleDisconnect = async (socket) => {
+  let disconnectedUser;
+
+  users.forEach((user) => {
+    if (socket.id === user.socketId) {
+      disconnectedUser = user;
+    }
+  });
+
+  console.log(disconnectedUser);
+
+  if (disconnectedUser) {
+    const userRoom = disconnectedUser.room;
+
+    const index = users.findIndex(
+      (user) => user.name === disconnectedUser.name
+    );
+    if (index !== -1) {
+      users.splice(index, 1);
+      console.log(
+        `ENDING handleDisconnect(): ${disconnectedUser.name} HAS LEFT THE ROOM ${disconnectedUser.room}`
+      );
+    }
+
+    socket.to(userRoom).emit("someoneleft", {
+      text: `${disconnectedUser.name} has left the room ${disconnectedUser.room}.`,
+      authortime: `Admin Says @${moment().format("h:mm:ss a")}`,
+      color: adminColor,
+    });
+  } else {
+    console.log(`disconnecting user failed`);
+  }
+};
+
+const handleTyping = (socket, clientData) => {
+  console.log("IN handleTyping()");
+  const name = clientData.from;
+  const index = users.findIndex((user) => user.name === name);
+  const room = users[index].room;
+
+  socket.to(room).emit("someoneistyping", { text: `${name} is typing...` });
+};
+
+const handleMessage = (io, socket, clientData) => {
+  console.log("IN handleMessage():");
+  const name = clientData.from;
+  const index = users.findIndex((user) => user.name === name);
+  const room = users[index].room;
+
+  const msg = clientData.text;
+  const authortime = `${name} Says @${moment().format("h:mm:ss a")}`;
+  console.log(`MESSAGE: ${msg}`);
+
+  //set color
+  let color = users.find((user) => user.name === name).color;
+  // console.log(`COLOUR: ${color}`);
+
+  // let coloridx = Math.floor(Math.random() * matColours.colours.length);
+  // let color = matColours.colours[coloridx];
+
+  io.in(room).emit("newmessage", {
+    text: msg,
+    authortime: authortime,
+    color: color,
+  });
+};
+
+export { handleJoin, handleDisconnect, handleTyping, handleMessage };
